37eeb047dc17546fa3628821b365d7e18c1d55fa
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-14 19:19:20 -0300

    web: clean App.tsx notes + test tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,8 @@
 import './styles/main.css';
 
-// Componentes + Propriedades
-// Componentes são compostos de funções que retornam HTML
-
-interface ButtonProps {
-  title?: string;
-}
-
-function Button(props: ButtonProps) {
-  return(
-    <button>
-      {props.title ? props.title : "Hello World"}
-    </button>
-  )
-}
-
 function App() {
   return (
-    <div>
-      <Button title="Enviar"/>
-      <Button/>
+    <div className="w-8 h-8 bg-violet-200">
     </div>
   )
 }
