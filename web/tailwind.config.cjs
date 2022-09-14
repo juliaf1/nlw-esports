7155ccd2606604aba34cc27919027e630c92ded2
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-14 19:44:29 -0300

    web: adds gradient css to tailwind config

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,8 +6,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+      },
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png')"
+        galaxy: "url('/background-galaxy.png')",
+        gradient: 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
       },
     },
   },
