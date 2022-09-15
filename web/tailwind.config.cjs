3827ef859d4d4eaec6fe749f6bd3a4746e9fa29d
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-14 21:49:43 -0300

    web: update tailwind config gradient styling

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -10,8 +10,8 @@ module.exports = {
       },
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
-        'game-gradient': 'linear-gradient(188deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.88%)',
+        'nlw-gradient': 'linear-gradient(90deg, #9572FC 20%, #43E7AD 40%, #E1D55D 80%)',
+        'game-gradient': 'linear-gradient(188deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 60%)',
       },
     },
   },
