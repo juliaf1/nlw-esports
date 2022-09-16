524bcf91258e75290fc62a93c05bbe07f9450a70
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-16 17:28:14 -0300

    web: update game card component whitespace indentation

--- web/src/components/GameCard.tsx
+++ web/src/components/GameCard.tsx
@@ -1,18 +1,18 @@
 interface GameCardProps {
-    bannerUrl: string;
-    title: string;
-    adsCount: number;
+  bannerUrl: string;
+  title: string;
+  adsCount: number;
 }
 
 export function GameCard(props: GameCardProps) {
-    return(
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src={props.bannerUrl} alt="" />
-          
-          <div className="w-full h-full px-4 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0 flex flex-col justify-end">
-            <strong className="font-bold text-white block">{ props.title }</strong>
-            <span className="text-zinc-300 text-sm block">{ props.adsCount } anúncio(s)</span>
-          </div>
-        </a>
-    )
+  return(
+    <a href="" className="relative rounded-lg overflow-hidden">
+      <img src={props.bannerUrl} alt="" />
+      
+      <div className="w-full h-full px-4 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0 flex flex-col justify-end">
+        <strong className="font-bold text-white block">{ props.title }</strong>
+        <span className="text-zinc-300 text-sm block">{ props.adsCount } anúncio(s)</span>
+      </div>
+    </a>
+  )
 };
