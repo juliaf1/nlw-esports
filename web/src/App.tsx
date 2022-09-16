ec5d62dcf97d883b14aab8df76a7d6f8c20cfc89
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-16 17:00:58 -0300

    web: imports create ad banner component in App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,8 @@
-import { MagnifyingGlassPlus } from 'phosphor-react';
-
 import './styles/main.css';
 
 import logoImg from './assets/logo.svg';
-
 import { GameCard } from './components/GameCard';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
 function App() {
   return (
@@ -16,26 +14,10 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameCard
-          bannerUrl=''
-          title=''
-          adsCount={1}
-        />
+        <GameCard bannerUrl="" title="" adsCount={1} />
       </div>
 
-      <div className="bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-16 pt-1">
-        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center flex-wrap">
-          <div>
-            <strong className="text-white text-2xl font-black block">Não encontrou o seu duo?</strong>
-            <span className="text-zinc-400 ">Publique um anúncio para encontrar outros jogadores.</span>
-          </div>
-
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center justify-center gap-2">
-            <MagnifyingGlassPlus size={24}/>
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      <CreateAdBanner />
     </div>
   )
 }
