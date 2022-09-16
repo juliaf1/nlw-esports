fbe51f815c34002f6e1f80c54ce13f2ac6641e3d
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-16 16:46:51 -0300

    server: imports ads controller functions to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,9 @@ import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 
 import { convertMinutesToTimeString, convertTimeStringToMinutes } from './utils/parse-time';
+import { consoleRequest } from './utils/log-request';
+
+import { createAd, getDiscord, listAds } from './controllers/ads_controller';
 
 const app = express();
 app.use(express.json());
@@ -32,77 +35,11 @@ app.get('/games', async (req, res) => {
     return res.json(games);
 });
 
-app.get('/games/:id/ads', async (req, res) => {
-    consoleRequest(req);
+app.get('/games/:id/ads', listAds);
 
-    const { id } = req.params;
+app.get('/ads/:id/discord', getDiscord);
 
-    const ads: any = await prisma.ad.findMany({
-        select: {
-            id: true,
-            name: true,
-            weekDays: true,
-            useVoiceChannel: true,
-            yearsPlaying: true,
-            hourStart: true,
-            hourEnd: true
-        },
-        where: {
-            gameId: id,
-        },
-        orderBy: {
-            createdAt: 'desc',
-        }
-    });
-
-    const formattedAd: any = ads.map ((ad: any) => {
-        return({
-            ...ad,
-            hourStart: convertMinutesToTimeString(ad.hourStart),
-            hourEnd: convertMinutesToTimeString(ad.hourEnd),
-        });
-    });
-
-    return res.json(formattedAd);
-});
-
-app.get('/ads/:id/discord', async (req, res) => {
-    consoleRequest(req);
-
-    const { id } = req.params;
-
-    const ad = await prisma.ad.findUniqueOrThrow({
-        select: {
-            discord: true,
-        },
-        where: { id },
-    });
-
-    return res.json({ discord: ad.discord });
-});
-
-app.post('/games/:id/ads', async (req, res) => {
-    consoleRequest(req);
-
-    const gameId = req.params.id;
-    const body = req.body;
-
-    const ad = await prisma.ad.create({
-        data: {
-            ...body,
-            hourStart: convertTimeStringToMinutes(body.hourStart),
-            hourEnd: convertTimeStringToMinutes(body.hourEnd),
-            gameId
-        }
-    });
-
-    return res.status(201).json(ad);
-});
-
-
-function consoleRequest(req: any) {
-    console.log(`GET ${req.url}`);
-};
+app.post('/games/:id/ads', createAd);
 
 app.listen(3333, () => {
     console.log('Up on 3333');
