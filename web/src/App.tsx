9673cbccee1f0831fa527c5c3b24b6759bcecb33
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-16 16:57:41 -0300

    web: imports game card component in App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,8 @@ import './styles/main.css';
 
 import logoImg from './assets/logo.svg';
 
+import { GameCard } from './components/GameCard';
+
 function App() {
   return (
     <div className="max-w-{1344px} mx-auto flex items-center flex-col py-20 px-20">
@@ -14,59 +16,11 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="game-1.png" alt="" />
-          
-          <div className="w-full h-full px-4 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0 flex flex-col justify-end">
-            <strong className="font-bold text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a href="" className="relative">
-          <img src="game-2.png" alt="" />
-          
-          <div className="w-full h-full px-4 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0 flex flex-col justify-end">
-            <strong className="font-bold text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a href="" className="relative">
-          <img src="game-3.png" alt="" />
-          
-          <div className="w-full h-full px-4 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0 flex flex-col justify-end">
-            <strong className="font-bold text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a href="" className="relative">
-          <img src="game-4.png" alt="" />
-          
-          <div className="w-full h-full px-4 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0 flex flex-col justify-end">
-            <strong className="font-bold text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a href="" className="relative">
-          <img src="game-5.png" alt="" />
-          
-          <div className="w-full h-full px-4 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0 flex flex-col justify-end">
-            <strong className="font-bold text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        
-        <a href="" className="relative">
-          <img src="game-6.png" alt="" />
-          
-          <div className="w-full h-full px-4 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0 flex flex-col justify-end">
-            <strong className="font-bold text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
+        <GameCard
+          bannerUrl=''
+          title=''
+          adsCount={1}
+        />
       </div>
 
       <div className="bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-16 pt-1">
