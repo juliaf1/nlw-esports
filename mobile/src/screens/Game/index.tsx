77a3ef66fe4e08014805436d8487a5dcac5159c8
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-20 14:44:28 -0300

    mobile: imports game params interface and useRoute to game screen

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,10 +1,15 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRoute } from '@react-navigation/native';
 
 import { styles } from './styles';
+import { GameParams } from '../../@types/navigation';
 
 import { Background } from '../../components/Background';
 
 export function Game() {
+  const route = useRoute();
+  const game = route.params as GameParams;
+
   return(
     <Background>
       <SafeAreaView></SafeAreaView>
