39c345ae18533447e7557d0238a3a9a31b8ecb24
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-20 14:29:43 -0300

    mobile: update game screen to use safe area view

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,9 +1,9 @@
-import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { styles } from './styles';
 
 export function Game() {
   return(
-    <View></View>
+    <SafeAreaView></SafeAreaView>
   );
 };
