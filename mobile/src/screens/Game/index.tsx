93a0408ac889f031f947d6a7dfe11a8071a44829
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-20 15:00:35 -0300

    mobile: adds game screen layout

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,10 +1,17 @@
+import { View, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRoute } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons';
 
+import logoImg from '../../assets/logo-nlw-esports.png';
+
+import { THEME } from '../../theme';
 import { styles } from './styles';
+
 import { GameParams } from '../../@types/navigation';
 
 import { Background } from '../../components/Background';
+import { Heading } from '../../components/Heading';
 
 export function Game() {
   const route = useRoute();
@@ -12,7 +19,29 @@ export function Game() {
 
   return(
     <Background>
-      <SafeAreaView></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Entypo
+              name="chevron-thin-left"
+              size={20}
+              color={THEME.COLORS.CAPTION_300}
+            />
+          </TouchableOpacity>
+
+          <Image
+            source={logoImg}
+            style={styles.logo}
+          />
+
+          <View style={styles.spacer} />
+        </View>
+
+        <Heading
+          title={game.title}
+          subtitle="Conecte-se e comece a jogar"
+        />
+      </SafeAreaView>
     </Background>
   );
 };
