2ebf0b0439c5cd0737f20de7f3e6611f2a711e3d
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-20 14:08:58 -0300

    mobile: update game card props based on server response

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType } from 'react-native';
+import { Text, TouchableOpacity, TouchableOpacityProps, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import { styles } from './styles';
@@ -6,9 +6,11 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number;
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -19,7 +21,7 @@ export function GameCard({ data, ...rest }: Props) {
   return(
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground
-        source={data.cover}
+        source={{uri: data.bannerUrl}}
         style={styles.cover}
       >
 
@@ -28,11 +30,11 @@ export function GameCard({ data, ...rest }: Props) {
           style={styles.footer}
         >
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
 
           <Text style={styles.ads}>
-            {data.ads} anúncios
+            {data._count.ads} anúncios
           </Text>
         </LinearGradient>
       </ImageBackground>
