c6287bcf25ebb6d9233e942967c31b7b0345a136
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-20 15:45:03 -0300

    mobile: adds duo card props and duo card layout

--- mobile/src/components/DuoCard/index.tsx
+++ mobile/src/components/DuoCard/index.tsx
@@ -2,8 +2,45 @@ import { View } from 'react-native';
 
 import { styles } from './styles';
 
-export function DuoCard() {
+import { DuoInfo } from '../DuoInfo';
+
+export interface DuoCardProps {
+  id: string;
+  name: string;
+  hourEnd: string;
+  hourStart: string;
+  useVoiceChannel: boolean;
+  weekDays: string[];
+  yearsPlaying: number;
+};
+
+interface Props {
+  data: DuoCardProps;
+};
+
+export function DuoCard({ data }: Props) {
   return(
-    <View></View>
+    <View style={styles.container}>
+      <DuoInfo
+        label="Nome"
+        value={data.name}
+      />
+
+      <DuoInfo
+        label="Tempo de jogo"
+        value={`${data.yearsPlaying} ano(s)`}
+      />
+
+      <DuoInfo
+        label="Disponibilidade"
+        value={`${data.weekDays.length} dia(s)`}
+      />
+
+      <DuoInfo
+        label="Chamada de áudio?"
+        value={data.useVoiceChannel ? 'Sim' : 'Não'}
+        colorValue={data.useVoiceChannel ? '#34D399' : '#F87171'}
+      />
+    </View>
   );
 };
