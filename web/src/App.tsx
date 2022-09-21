3a3072cfec1beb74cede8aa7b520960739a77f54
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-21 14:08:30 -0300

    web: refactor ad form component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,7 +6,7 @@ import './styles/main.css';
 import logoImg from './assets/logo.svg';
 import { GameCard } from './components/GameCard';
 import { CreateAdBanner } from './components/CreateAdBanner';
-import { CreateAdForm } from './components/Form/CreateAdForm';
+import { CreateAdModal } from './components/CreateAdModal';
 
 // useEffect(() => {}, []); useEffect syntax structure
 // parameters: result, [list of useStates that trigger that result]
@@ -55,16 +55,7 @@ function App() {
 
       <Dialog.Root>
         <CreateAdBanner />
-
-        <Dialog.Portal>
-          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-
-          <Dialog.Content className="bg-[#2A2634] fixed py-8 px-10 rounded-lg w-[480px] text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-lg shadow-black/25">
-            <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
-
-            <CreateAdForm />
-          </Dialog.Content>
-        </Dialog.Portal>
+        <CreateAdModal />
       </Dialog.Root>
     </div>
   )
