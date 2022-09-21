027ec73863872d9476f5038b86ca10680a86008a
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-21 15:04:44 -0300

    web: updates indentation on ad banner + removes nested buttons from dialog trigger

--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -2,21 +2,19 @@ import { MagnifyingGlassPlus } from 'phosphor-react';
 import * as Dialog from '@radix-ui/react-dialog';
 
 export function CreateAdBanner() {
-    return(
-        <div className="bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-16 pt-1">
-            <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center flex-wrap">
-                <div>
-                    <strong className="text-white text-2xl font-black block">Não encontrou o seu duo?</strong>
-                    <span className="text-zinc-400 ">Publique um anúncio para encontrar outros jogadores.</span>
-                </div>
-
-                <Dialog.Trigger>
-                    <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center justify-center gap-2">
-                        <MagnifyingGlassPlus size={24}/>
-                        Publicar anúncio
-                    </button>
-                </Dialog.Trigger>
-            </div>
+  return(
+    <div className="bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-16 pt-1">
+      <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center flex-wrap">
+        <div>
+          <strong className="text-white text-2xl font-black block">Não encontrou o seu duo?</strong>
+          <span className="text-zinc-400 ">Publique um anúncio para encontrar outros jogadores.</span>
         </div>
-    )
+
+        <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center justify-center gap-2">
+          <MagnifyingGlassPlus size={24}/>
+          Publicar anúncio
+        </Dialog.Trigger>
+      </div>
+    </div>
+  );
 };
