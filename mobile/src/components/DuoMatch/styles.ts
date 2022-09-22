b55f9cc5dc9d8215f9606bf6aad26f50746d8f53
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-22 13:23:10 -0300

    mobile: adds duo match styling for content card

--- mobile/src/components/DuoMatch/styles.ts
+++ mobile/src/components/DuoMatch/styles.ts
@@ -7,10 +7,23 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: THEME.COLORS.OVERLAY,
+  },
+  content: {
+    width: 311,
+    backgroundColor: THEME.COLORS.SHAPE,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
   },
   discord: {
     color: THEME.COLORS.TEXT,
     fontSize: THEME.FONT_SIZE.MD,
     fontFamily: THEME.FONT_FAMILY.REGULAR,
+  },
+  close: {
+    alignSelf: 'flex-end',
+    // margin: 16
   },
 });
