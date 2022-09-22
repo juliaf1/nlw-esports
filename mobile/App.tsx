6a122e2c73fa0ee5111d9b8bbd50eb4d60685eb1
Julia Frederico <magalhaesjulia@id.uff.br>
2022-09-22 14:09:21 -0300

    mobile: adds structure to send push notification

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,7 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
 import {
   useFonts,
   Inter_400Regular,
@@ -15,8 +17,6 @@ import { Loading } from './src/components/Loading';
 import './src/services/notificationConfig';
 import { getPushNotificationToken } from './src/services/getPushNotificationToken';
 
-// const getNotificationListener = useRef<>();
-
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -24,6 +24,30 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  });
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    };
+  }, []);
 
   return (
     <Background>
